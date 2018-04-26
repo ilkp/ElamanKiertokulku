d51563a0c2934f506802b5f103fba9b0308e6b01
niko <niko@niko-HP.wlan.metropolia.fi>
2018-04-26 12:48:48 +0300

    Kello lisätty

--- src/elämänkiertokulku/ElämänKiertokulkuTesti.java
+++ src/elämänkiertokulku/ElämänKiertokulkuTesti.java
@@ -1,12 +1,16 @@
 
 package elämänkiertokulku;
 
+import elämänkiertokulku.kontrolleri.Kello;
 import elämänkiertokulku.kontrolleri.Kontrolleri;
 import elämänkiertokulku.simulaatio.Elain;
 import elämänkiertokulku.simulaatio.Kartta;
 import elämänkiertokulku.simulaatio.Lauma;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,7 +20,7 @@ import javafx.stage.Stage;
 
 public class ElämänKiertokulkuTesti {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         Kontrolleri kontrolleri = new Kontrolleri(3, 3);
         
         kontrolleri.getKartta().alusta();
@@ -31,6 +35,10 @@ public class ElämänKiertokulkuTesti {
         });
         
         kontrolleri.getKartta().piirraKartta();
+        
+        Kello kello = new Kello();
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(kello.getRunnable(), 0, 1, TimeUnit.SECONDS);
         
     }
 }
--- src/elämänkiertokulku/kontrolleri/Kello.java
+++ src/elämänkiertokulku/kontrolleri/Kello.java
@@ -8,26 +8,8 @@ import java.util.List;
 
 public class Kello {
     Kontrolleri kontrolleri = new Kontrolleri(3,3);
-    protected Runnable helloRunnable = new Runnable() {
-/*/
-            kontrolleri.getKartta().alusta();
-  
-            kontrolleri.lisaaLauma(new Lauma(kontrolleri.seuraavaId(), kontrolleri.getKartta().getRuudut()[1][1]));
-            
-            List<Lauma> laumat = kontrolleri.getLaumat();
-            if (laumat.size() == 1){
-                    kontrolleri.lisaaLauma(new Lauma(kontrolleri.seuraavaId(), kontrolleri.getKartta().getRuudut()[0][0]));
-                }
-            laumat.forEach((lauma) -> {
-                lauma.lisaaJasen(new Elain(kontrolleri.seuraavaId(), lauma.getRuutu()));
-            }
-            
-            );
-            /*/
-        @Override
-        public void run() {
-            kontrolleri.getKartta().piirraKartta();
-            }
+    Runnable helloRunnable = () -> {
+        System.out.println("hello");
     };
     public Runnable getRunnable() {
         return this.helloRunnable;
